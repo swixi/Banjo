1fbb6a0776a1d60ae5a17ba9323bba5bb2379faf
Brian <swixee@gmail.com>
2020-05-02 23:17:12 -0400

    initial commit

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float force = 10f;
+    public Rigidbody ballRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // FixedUpdate is good for physics apparently
+    void FixedUpdate()
+    {
+        
+        if(Input.GetKey("w") || Input.GetKey("up"))
+            ballRB.AddForce(0, 0, Time.deltaTime * force, ForceMode.Impulse);
+        if(Input.GetKey("a") || Input.GetKey("left"))
+            ballRB.AddForce(-(Time.deltaTime * force), 0, 0, ForceMode.Impulse);
+        if(Input.GetKey("s") || Input.GetKey("down"))
+            ballRB.AddForce(0, 0, -(Time.deltaTime * force), ForceMode.Impulse);
+        if(Input.GetKey("d") || Input.GetKey("right"))
+            ballRB.AddForce(Time.deltaTime * force, 0, 0, ForceMode.Impulse);    
+        
+
+    }
+}
